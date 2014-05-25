1f4c2c5e354c70035d7f307e6c74272512e3b8b7
Andrew Hao <ahao@blurb.com>
2014-05-25 15:22:06 -0700

    Switch to gulp-spawn-mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require('gulp');
-var mocha = require('gulp-mocha');
+var mocha = require('gulp-spawn-mocha');
 
 gulp.task("test", function() {
-  require('coffee-script/register');
-  gulp.src("test/**/*.coffee")
-  .pipe(mocha({
-    reporter: "spec"
-  }));
+  console.log("Starting test.");
+  return test().on('error', function(e) {
+    throw e;
+  });
 });
 
 gulp.task("default", function() {
   console.log("Do the default thing!");
 });
+
+var test = function() {
+  console.log("entering test()");
+  return gulp.src(["test/**/*.coffee"]).pipe(mocha({
+    bin: "node_modules/.bin/mocha",
+    reporter: "spec",
+    compilers: "coffee:coffee-script/register"
+  })).on("error", console.warn.bind(console));
+};
+
